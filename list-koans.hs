19a0f4bcfa055ec54d69aaee8edb1a4358006baa
Feihong Hsu <feihong.hsu@gmail.com>
2017-04-16 13:28:40 -0500

    Added more list koans

--- list-koans.hs
+++ list-koans.hs
@@ -5,14 +5,34 @@
 -}
 import Test.Hspec
 
+
 main :: IO ()
 main = hspec $ do
   describe "List koans" $ do
+    it "null" $ do
+      null [] `shouldBe` True
+
+    it "init" $ do
+      init [1..6] `shouldBe` [1, 2, 3, 4, 5]
+
+    it "last" $ do
+      last [1..6] `shouldBe` 6
+
     it "head" $ do
       head [1, 2, 3] `shouldBe` (1 :: Int)
 
+    it "head on empty list" $ do
+      head [] `shouldThrow` anyErrorCall
+
     it "tail" $ do
       tail [1, 2, 3] `shouldBe` [2, 3]
+
+    -- it "tail on empty list" $ do
+    --   tail [] `shouldThrow` anyErrorCall
+
+    it "tail on single-element list" $ do
+      tail [1] `shouldBe` []
+      tail [1] `shouldSatisfy` null
 
     it "map" $ do
       map (\n -> n + 1) [1, 2, 3] `shouldBe` [2, 3, 4]
