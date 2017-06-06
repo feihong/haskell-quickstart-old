5c90e0996069ceb7aa8a912712a20c5d0ba2c55e
Feihong Hsu <feihong.hsu@gmail.com>
2017-06-06 09:25:39 -0500

    Add unfold and treeBuild functions

--- tree.hs
+++ tree.hs
@@ -21,6 +21,20 @@ foldTree :: (a -> b -> b) -> b -> BinaryTree a -> b
 foldTree f z tree = foldr f z $ inorder tree
 
 
+unfold :: (a -> Maybe (a, b, a)) -> a -> BinaryTree b
+unfold f x =
+  case f x of
+    Nothing -> Leaf
+    Just (a1, b, a2) -> Node (unfold f a1) b (unfold f a2)
+
+
+treeBuild :: Integer -> BinaryTree Integer
+treeBuild = unfold (\a ->
+  case a of
+    0 -> Nothing
+    x -> Just (x-1, x-1, x-1))
+
+
 tree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
 
 
@@ -32,3 +46,19 @@ main = hspec $ do
 
     it "foldTree" $ do
       foldTree ((:) . (*2)) [] tree `shouldBe` [2,4,6]
+
+    it "unfold" $ do
+      let
+        f 0 = Just (1, 0, 1)
+        f 1 = Just (2, 1, 2)
+        f _ = Nothing
+
+      unfold f 2 `shouldBe` Leaf
+      unfold f 1 `shouldBe` Node Leaf 1 Leaf
+      unfold f 0 `shouldBe` Node (Node Leaf 1 Leaf) 0 (Node Leaf 1 Leaf)
+
+    it "treeBuild" $ do
+      treeBuild 0 `shouldBe` Leaf
+      treeBuild 1 `shouldBe` Node Leaf 0 Leaf
+      treeBuild 2 `shouldBe` Node (Node Leaf 0 Leaf) 1 (Node Leaf 0 Leaf)
+      treeBuild 3 `shouldBe` Node (Node (Node Leaf 0 Leaf) 1 (Node Leaf 0 Leaf)) 2 (Node (Node Leaf 0 Leaf) 1 (Node Leaf 0 Leaf))
