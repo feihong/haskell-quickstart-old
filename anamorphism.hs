c83cdcc96b2bfae3c5ae51f80696b02507b1c335
Feihong Hsu <feihong.hsu@gmail.com>
2017-06-05 20:23:24 -0500

    Add anamorphism exercises

---
+++ anamorphism.hs
@@ -0,0 +1,20 @@
+{- stack
+  script
+  --resolver lts-8.11
+  --package hspec
+-}
+
+
+myIterate :: (a -> a) -> a -> [a]
+myIterate f x = x : myIterate f (f x)
+
+
+myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+myUnfoldr f x =
+  case f x of
+    Nothing -> []
+    Just (a, b) -> a : myUnfoldr f b
+
+
+betterIterate :: (a -> a) -> a -> [a]
+betterIterate f = myUnfoldr (\b -> Just (b, f b))
