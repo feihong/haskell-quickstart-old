dc75e138369ca3c4bc72dfef1ef2f42fc60c000e
Feihong Hsu <feihong.hsu@gmail.com>
2017-06-09 18:18:07 -0500

    Add List and TalkToMe functor instances

--- functor.hs
+++ functor.hs
@@ -34,7 +34,37 @@ data Parappa f g a =
   deriving (Eq, Show)
 
 instance (Functor f, Functor g) => Functor (Parappa f g) where
-  fmap h (DaWrappa fa ga) = DaWrappa  (fmap h fa) (fmap h ga)
+  fmap h (DaWrappa fa ga) = DaWrappa (fmap h fa) (fmap h ga)
+
+
+data List a =
+    Nil
+  | Cons a (List a)
+  deriving (Eq, Show)
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons h t) = Cons (f h) (fmap f t)
+
+
+data TalkToMe a =
+    Halt
+  | Print String a
+  | Read (String -> a)
+
+instance Show a => Show (TalkToMe a) where
+  show Halt = "Halt"
+  show (Print s a) = "Print " ++ s ++ " " ++ show a
+  show (Read f) = "Read fn"
+
+instance Functor TalkToMe where
+  fmap _ Halt = Halt
+  fmap f (Print s a) = Print s (f a)
+  fmap f (Read g) = Read (f . g)
+
+-- fmap (+1) Halt ~ Halt
+-- fmap (+1) (Print "abc" 55) ~ Print "abc" 56
+-- fmap show (Read read) ~ ?
 
 
 main :: IO ()
@@ -51,3 +81,7 @@ main = hspec $ do
     it "Parappa" $ do
       fmap (+1) (DaWrappa [44] (Just 55)) `shouldBe` DaWrappa [45] (Just 56)
       fmap (+1) (DaWrappa [] Nothing) `shouldBe` DaWrappa [] Nothing
+
+    it "List" $ do
+      fmap (+1) Nil `shouldBe` Nil
+      fmap (+1) (Cons 5 Nil) `shouldBe` (Cons 6 Nil)
