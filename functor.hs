dcff5cefc4312cf0887acc0d73e59a9e43ecf750
Feihong Hsu <feihong.hsu@gmail.com>
2017-06-09 00:25:21 -0500

    Add LiftItOut and Parappa functor instances

--- functor.hs
+++ functor.hs
@@ -21,6 +21,22 @@ instance Functor (Quant a) where
 type QuantCI = Quant Char Int
 
 
+data LiftItOut f a =
+  LiftItOut (f a)
+  deriving (Eq, Show)
+
+instance Functor f => Functor (LiftItOut f) where
+  fmap f (LiftItOut fa) = LiftItOut (fmap f fa)
+
+
+data Parappa f g a =
+  DaWrappa (f a) (g a)
+  deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+  fmap h (DaWrappa fa ga) = DaWrappa  (fmap h fa) (fmap h ga)
+
+
 main :: IO ()
 main = hspec $ do
   describe "Functor instance exercises" $ do
@@ -28,3 +44,10 @@ main = hspec $ do
       fmap (+1) Finance `shouldBe` (Finance :: QuantCI)
       fmap (+1) (Desk 'c') `shouldBe` (Desk 'c' :: QuantCI)
       fmap (+1) (Bloor 55) `shouldBe` (Bloor 56 :: QuantCI)
+
+    it "LiftItOut" $ do
+      fmap (*2) (LiftItOut [4]) `shouldBe` LiftItOut [8]
+
+    it "Parappa" $ do
+      fmap (+1) (DaWrappa [44] (Just 55)) `shouldBe` DaWrappa [45] (Just 56)
+      fmap (+1) (DaWrappa [] Nothing) `shouldBe` DaWrappa [] Nothing
