ef5c0dd2328f19b9b1d265105fec442db1453e39
Feihong Hsu <feihong.hsu@gmail.com>
2017-06-09 00:13:43 -0500

    Add functor instance exercise

--- functor.hs
+++ functor.hs
@@ -1,3 +1,4 @@
+#!/usr/bin/env stack
 {- stack
   script
   --resolver lts-8.11
@@ -6,26 +7,24 @@
 import Test.Hspec
 
 
+data Quant a b =
+    Finance
+  | Desk a
+  | Bloor b
+  deriving (Eq, Show)
+
+instance Functor (Quant a) where
+  fmap _ Finance = Finance
+  fmap _ (Desk x) = Desk x
+  fmap f (Bloor x) = Bloor $ f x
+
+type QuantCI = Quant Char Int
+
+
 main :: IO ()
 main = hspec $ do
-  describe "Functor" $ do
-    it "Lift twice" $ do
-      (fmap . fmap) (++"lol") $ Just ["Hi,", "Hello"]
-      `shouldBe` Just ["Hi,lol","Hellolol"]
-
-    it "Lift over function" $ do
-      let c = fmap (*2) (\x -> x - 2)
-      c 1 `shouldBe` -2
-
-    it "Lift over another function" $ do
-      let d = fmap ((return '1' ++) . show) (\x -> [x, 1..3])
-      d 0 `shouldBe` "1[0,1,2,3]"
-
-    it "Lift over IO" $ do
-      let e :: IO Integer
-          e = let ioi = readIO "1" :: IO Integer
-                  changed = fmap read $ fmap ("123"++) $ fmap show ioi 
-              in fmap (*3) changed
-
-      -- No idea how to do a comparison with IO values, so just fudge it
-      True `shouldBe` True
+  describe "Functor instance exercises" $ do
+    it "Quant" $ do
+      fmap (+1) Finance `shouldBe` (Finance :: QuantCI)
+      fmap (+1) (Desk 'c') `shouldBe` (Desk 'c' :: QuantCI)
+      fmap (+1) (Bloor 55) `shouldBe` (Bloor 56 :: QuantCI)
