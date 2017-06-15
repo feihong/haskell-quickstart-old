c6625799161b83804972b5cf7914d5291bae7431
Feihong Hsu <feihong.hsu@gmail.com>
2017-06-15 15:02:52 -0500

    Add examples for Maybe monad and Either monad

--- monad-behavior.hs
+++ monad-behavior.hs
@@ -5,6 +5,7 @@
 -}
 import Test.Hspec
 
+
 twiceWhenEven :: [Integer] -> [Integer]
 twiceWhenEven xs = do
   x <- xs
@@ -20,6 +21,8 @@ twiceWhenEven' xs =
     then [x*x, x*x]
     else [x]
 
+type ESI = Either String Integer
+
 
 main :: IO ()
 main = hspec $ do
@@ -27,3 +30,23 @@ main = hspec $ do
     it "twiceWhenEven" $ do
       twiceWhenEven [1..8] `shouldBe` [1,4,4,3,16,16,5,36,36,7,64,64]
       twiceWhenEven' [1..8] `shouldBe` [1,4,4,3,16,16,5,36,36,7,64,64]
+
+    it "Maybe" $ do
+      let nothing :: Maybe Integer
+          nothing = Nothing
+
+      (Just 4 >>= (\x -> Just 6)) `shouldBe` Just 6
+      (Just 4 >>= (\x -> Nothing)) `shouldBe` nothing
+      (Nothing >>= (\x -> Nothing)) `shouldBe` nothing
+      (Nothing >>= (\x -> Just 6)) `shouldBe` nothing
+
+    it "Either" $ do
+      let input :: Either String Integer
+          input = Right 8
+          err :: Either String Double
+          err = Left "err"
+
+      (input >>= (\x -> Right 3)) `shouldBe` Right 3
+      (input >>= (\x -> err)) `shouldBe` Left "err"
+      (err >>= (\x -> err)) `shouldBe` Left "err"
+      (err >>= (\x -> Right 4)) `shouldBe` Left "err"
