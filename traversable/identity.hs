f84e5ceb4699232cec364d9bc4ac30d62a2305bb
Feihong Hsu <feihong.hsu@gmail.com>
2017-06-29 00:14:49 -0500

    Add quickcheck tests for Identity traversable instance

--- traversable/identity.hs
+++ traversable/identity.hs
@@ -2,8 +2,14 @@
   script
   --resolver lts-8.11
   --package hspec
+  --package QuickCheck
+  --package checkers
 -}
 import Test.Hspec
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
 
 newtype Identity a = Identity a deriving (Eq, Ord, Show)
 
@@ -13,9 +19,16 @@ instance Functor Identity where
 instance Foldable Identity where
   foldMap f (Identity x) = f x
 
--- traverse :: (Applicative f, Traversable t) => (a -> f b) -> t a -> f (t b)
+instance Applicative Identity where
+  pure = Identity
+  Identity f <*> Identity x = Identity (f x)
+
+
 instance Traversable Identity where
+  -- traverse :: (Applicative f, Traversable t) => (a -> f b) -> t a -> f (t b)
   traverse f (Identity x) = fmap Identity (f x)
+  -- sequenceA :: (Applicative f, Traversable t) => t (f a) -> f (t a)
+  sequenceA (Identity x) = fmap Identity x
 
 
 tests = hspec $ do
@@ -29,6 +42,19 @@ tests = hspec $ do
     it "with list" $ do
       traverse (\x -> [x]) (Identity 4) `shouldBe` [Identity 4]
 
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = do
+    x <- arbitrary
+    return $ Identity x
+
+instance Eq a => EqProp (Identity a) where
+  (=-=) = eq
+
+
 main :: IO ()
 main = do
   tests
+
+  let trigger = undefined :: Identity (String, Char, [Int])
+  quickBatch (traversable trigger)
