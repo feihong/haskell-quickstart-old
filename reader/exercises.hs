3a93c0a20f2065a0e0c676a503b439c0a1d2a002
Feihong Hsu <feihong.hsu@gmail.com>
2017-07-01 11:47:20 -0500

    Add Reader Monad

--- reader/exercises.hs
+++ reader/exercises.hs
@@ -32,13 +32,24 @@ instance Functor (Reader r) where
   fmap :: (a -> b) -> Reader r a -> Reader r b
   fmap f (Reader ra) = Reader $ f . ra
 
-instance Applicative (Reader  r) where
+instance Applicative (Reader r) where
   pure :: a -> Reader r a
   pure a = Reader $ \_ -> a
 
   (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
   Reader rab <*> Reader ra =
-    Reader $ \r -> (rab r) (ra r)
+    Reader $ \r -> let ab = rab r
+                       a = ra r
+                   in ab a    -- return instance of type b
+
+instance Monad (Reader r) where
+  return = pure
+
+  (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
+  Reader ra >>= aRb =
+    Reader $ \r -> let a = ra r    -- type a
+                       rb = aRb a  -- type Reader r b
+                   in (runReader rb) r    -- type b
 
 
 ask :: Reader a a
